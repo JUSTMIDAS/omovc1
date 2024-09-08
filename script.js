66f6aa05b5f17d270444d0323dcca87f44d55e63
JUSTMIDAS <128001700+JUSTMIDAS@users.noreply.github.com>
2024-09-08 18:46:05 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const container = document.querySelector('.third');
-const scrollDistance = 50; // Adjust the scroll distance as needed
-let currentPosition = 0;
+ const container = document.querySelector('.third');
+const scrollDistance = 100; // Adjust the scroll distance as needed
+ let currentPosition = 0;
 
 function scrollDivs() {
   currentPosition += scrollDistance;
@@ -8,10 +9,9 @@ function scrollDivs() {
     currentPosition = 0;
   }
   
-  container.scrollTo({
+   container.scrollTo({
     left: currentPosition,
-    behavior: 'smooth'
-  });
-}
+    behavior: 'smooth'   });
+ }
+ setInterval(scrollDivs, 1000); // Adjust the interval time (in milliseconds) as needed
 
-setInterval(scrollDivs, 1000); // Adjust the interval time (in milliseconds) as needed
